481e7ad518d82ab3dc5fbcc8c22076fb6f3f7607
chinoy <chinoy316@gmail.com>
2024-10-05 19:12:46 +0800

    chore: changed db name.

--- stylink/include/config.php
+++ stylink/include/config.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "Stylink_Up";
+$dbname = "stylink";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
