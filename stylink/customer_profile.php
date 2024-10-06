2ecd2a2cfa8558cb6deba844449eeba17ff5e1a6
chinoy <chinoy316@gmail.com>
2024-10-07 00:28:41 +0800

    feat: removed old footers in the following pages.

--- stylink/customer_profile.php
+++ stylink/customer_profile.php
@@ -595,79 +595,9 @@ ob_end_flush();
             </div>
         </div>
         <!-- Li's Main Blog Page Area End Here -->
-        <!-- Begin Footer Area -->
-        <div class="footer">
-            <!-- Begin Footer Static Top Area -->
-            <div class="footer-static-top">
-                <div class="container">
-                    <!-- Begin Footer Shipping Area -->
-                    <div class="footer-shipping pt-60 pb-55 pb-xs-25">
-                        <div class="row">
-                            <!-- Begin Li's Shipping Inner Box Area -->
-                            <div class="col-lg-3 col-md-6 col-sm-6 pb-sm-55 pb-xs-55">
-                                <div class="li-shipping-inner-box">
-                                    <div class="shipping-icon">
-                                        <img src="images/shipping-icon/1.png" alt="Shipping Icon">
-                                    </div>
-                                    <div class="shipping-text">
-                                        <h2>Free Delivery</h2>
-                                        <p>And free returns. See checkout for delivery dates.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- Li's Shipping Inner Box Area End Here -->
-                            <!-- Begin Li's Shipping Inner Box Area -->
-                            <div class="col-lg-3 col-md-6 col-sm-6 pb-sm-55 pb-xs-55">
-                                <div class="li-shipping-inner-box">
-                                    <div class="shipping-icon">
-                                        <img src="images/shipping-icon/2.png" alt="Shipping Icon">
-                                    </div>
-                                    <div class="shipping-text">
-                                        <h2>Safe Payment</h2>
-                                        <p>Pay with the world's most popular and secure payment methods.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- Li's Shipping Inner Box Area End Here -->
-                            <!-- Begin Li's Shipping Inner Box Area -->
-                            <div class="col-lg-3 col-md-6 col-sm-6 pb-xs-30">
-                                <div class="li-shipping-inner-box">
-                                    <div class="shipping-icon">
-                                        <img src="images/shipping-icon/3.png" alt="Shipping Icon">
-                                    </div>
-                                    <div class="shipping-text">
-                                        <h2>Shop with Confidence</h2>
-                                        <p>Our Buyer Protection covers your purchasefrom click to delivery.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- Li's Shipping Inner Box Area End Here -->
-                            <!-- Begin Li's Shipping Inner Box Area -->
-                            <div class="col-lg-3 col-md-6 col-sm-6 pb-xs-30">
-                                <div class="li-shipping-inner-box">
-                                    <div class="shipping-icon">
-                                        <img src="images/shipping-icon/4.png" alt="Shipping Icon">
-                                    </div>
-                                    <div class="shipping-text">
-                                        <h2>24/7 Help Center</h2>
-                                        <p>Have a question? Call a Specialist or chat online.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- Li's Shipping Inner Box Area End Here -->
-                        </div>
-                    </div>
-                    <!-- Footer Shipping Area End Here -->
-                </div>
-            </div>
 
-            <!-- Footer Static Middle Area End Here -->
-            <!-- Begin Footer Static Bottom Area -->
-            <!-- Begin Footer Area -->
-            <?php include("include/footer.php"); ?>
-            <!-- Footer Static Bottom Area End Here -->
-        </div>
-        <!-- Footer Area End Here -->
+        <?php include("include/footer.php"); ?>
+
     </div>
     <!-- Body Wrapper End Here -->
     <!-- jQuery-V1.12.4 -->
